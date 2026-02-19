f7bc4d1d8b6e8817155536135d316eeaf7435181
Hamim Ahmed <hamim.ahmed541@gmail.com>
2026-02-19 23:49:37 +0600

    day 89 added

---
+++ Day_89.py
@@ -0,0 +1,9 @@
+# what is the use of split and join function of str ?
+
+# split() and join() in Python (String Methods) These two functions are used for breaking strings and combining strings.
+
+
+# split() → Break a String into a List
+text = "I love Python"
+words = text.split()
+print(words)
